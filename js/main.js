7d0dfb82b85a26cea09172ed521985dc5d1d6a16
Asya Lebedeva <star@asyaspace.com>
2026-06-20 00:36:08 +0700

    refactor: old version website

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict'
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
